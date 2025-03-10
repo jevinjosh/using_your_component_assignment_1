9bc14b6aa2f8e6156301192b6529d25f67a3c192
jevin josh <jevinjosh000@gmail.com>
2025-03-10 10:05:59 +0530

    update

---
+++ vite-project/src/components/bookcard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const BookCard=({book})=>{
+    return(
+        <div className="book-card">
+            <img src={book.image} alt={book.name} className="book-cover"></img>
+            <h3 className="book-title">{book.name}</h3>
+            <p className="book-author">Author: {book.author}</p>
+            <p className="book-genre">Genre: {book.genre}</p>
+        </div>
+    );
+};
+
+export default BookCard;
